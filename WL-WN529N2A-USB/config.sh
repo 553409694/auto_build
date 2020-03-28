1ae998465b5f338888f879c66df5a64b1dc57fd6
553409694 <553409694@qq.com>
2020-03-28 23:24:50 +0800

    ##padavan_hanwckf add wl-wn529n2a and wl-wn529n2a-usb##1.5M-STORAGE##add https support

---
+++ WL-WN529N2A-USB/config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /opt/rt-n56u
+cp -r /home/runner/work/auto_build/auto_build/WL-WN529N2A-USB/WL-WN529N2A-USB trunk/configs/boards/
+cp /home/runner/work/auto_build/auto_build/WL-WN529N2A-USB/WL-WN529N2A-USB.config trunk/configs/templates/
---
+++ WL-WN529N2A/config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /opt/rt-n56u
+cp -r /home/runner/work/auto_build/auto_build/WL-WN529N2A/WL-WN529N2A trunk/configs/boards/
+cp /home/runner/work/auto_build/auto_build/WL-WN529N2A/WL-WN529N2A.config trunk/configs/templates/
