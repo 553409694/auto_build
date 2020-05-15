79df320ae175d309b7fd6d511954b4588a3c86a9
553409694 <553409694@qq.com>
2020-05-15 14:25:06 +0800

    ##padavan_hanwckf build k2p-nano-5.0##add openvpn

--- K2P_nano-5.0/config.sh
+++ K2P_nano-5.0/config.sh
@@ -4,3 +4,4 @@ cd /opt/rt-n56u
 cp /home/runner/work/auto_build/auto_build/K2P_nano-5.0/kernel-3.4.x-5.0.config trunk/configs/boards/K2P/kernel-3.4.x-5.0.config
 sed -i "s/CONFIG_FIRMWARE_INCLUDE_HTTPS=n/CONFIG_FIRMWARE_INCLUDE_HTTPS=y/g" trunk/configs/templates/K2P_nano-5.0.config
 sed -i "s/CONFIG_FIRMWARE_INCLUDE_OPENSSL_EXE=n/CONFIG_FIRMWARE_INCLUDE_OPENSSL_EXE=y/g" trunk/configs/templates/K2P_nano-5.0.config
+sed -i "s/CONFIG_FIRMWARE_INCLUDE_OPENVPN=n/CONFIG_FIRMWARE_INCLUDE_OPENVPN=y/g" trunk/configs/templates/K2P_nano-5.0.config
